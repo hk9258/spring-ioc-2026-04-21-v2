c80630fca4182d66837f009eda61d6ffe2a34fb5
hk9258 <jhg9258@gmail.com>
2026-05-04 09:33:42 +0900

    t2 - testPostService 빈 생성 성공 (Green), t3 - 싱글톤 검증 실패 (Red)

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,15 +1,68 @@
 package com.ll.framework.ioc;
 
-// 기본 코드 : t1 (green) , t2 (red)
-public class ApplicationContext {
-    public ApplicationContext(String basePackage) {
+import com.ll.framework.ioc.annotations.Component;
+import com.ll.framework.ioc.annotations.Configuration;
+import com.ll.framework.ioc.annotations.Repository;
+import com.ll.framework.ioc.annotations.Service;
+import org.reflections.Reflections;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+import java.util.HashSet;
+import java.util.Set;
+
+// "t2 - testPostService 빈 생성 성공 (Green)"
+public class ApplicationContext {
+
+    private final String basePackage;
+
+    public ApplicationContext(String basePackage) {
+        this.basePackage = basePackage;
     }
 
     public void init() {
     }
 
     public <T> T genBean(String beanName) {
+        Reflections reflections = new Reflections(basePackage);
+
+        Set<Class<?>> classes = new HashSet<>();
+        classes.addAll(reflections.getTypesAnnotatedWith(Component.class));
+        classes.addAll(reflections.getTypesAnnotatedWith(Service.class));
+        classes.addAll(reflections.getTypesAnnotatedWith(Repository.class));
+        classes.addAll(reflections.getTypesAnnotatedWith(Configuration.class));
+
+        for (Class<?> clazz : classes) {
+            if (clazz.isAnnotation() || clazz.isInterface() || Modifier.isAbstract(clazz.getModifiers())) {
+                continue;
+            }
+
+            String currentBeanName = lowerFirst(clazz.getSimpleName());
+
+            if (currentBeanName.equals(beanName)) {
+                return (T) createInstance(clazz);
+            }
+        }
+
         return null;
+    }
+
+    private Object createInstance(Class<?> clazz) {
+        try {
+            Constructor<?> constructor = clazz.getDeclaredConstructors()[0];
+            constructor.setAccessible(true);
+
+            Class<?>[] parameterTypes = constructor.getParameterTypes();
+            Object[] args = new Object[parameterTypes.length];
+
+            return constructor.newInstance(args);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String lowerFirst(String simpleName) {
+        return Character.toLowerCase(simpleName.charAt(0)) + simpleName.substring(1);
     }
 }
